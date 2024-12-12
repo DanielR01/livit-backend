3017cc3dbeacff98d7b2669eaf00d4953ec7e9fe
Daniel Rojas Asis <danielrojasasis@gmail.com>
2024-12-12 15:50:13 -0500

    N

--- functions/src/errors.ts
+++ functions/src/errors.ts
@@ -12,4 +12,18 @@ class UserAlreadyExistsError extends Error {
   }
 }
 
-export { UsernameAlreadyTakenError, UserAlreadyExistsError };
+class MissingParametersError extends Error {
+  constructor() {
+    super("Missing parameters");
+    this.name = "MissingParametersError";
+  }
+}
+
+class NotValidUserDataError extends Error {
+  constructor() {
+    super("Not valid user data");
+    this.name = "NotValidUserDataError";
+  }
+}
+
+export { UsernameAlreadyTakenError, UserAlreadyExistsError, MissingParametersError, NotValidUserDataError };
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,7 +1,6 @@
 import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
-//const  {log}= require("firebase-functions/logger");
-import { UsernameAlreadyTakenError, UserAlreadyExistsError } from "./errors";
+import { UsernameAlreadyTakenError, UserAlreadyExistsError, MissingParametersError, NotValidUserDataError } from "./errors";
 
 admin.initializeApp();
 
@@ -9,59 +8,82 @@ const db = admin.firestore();
 
 //Function to create user and username in a transaction
 export const createUserAndUsername = functions.https.onCall(async (request) => {
-  const {userId, username, userType, name} = request.data;
-
-  if (!userId) {
-    throw new functions.https.HttpsError("invalid-argument", "Missing userId");
-  }
-  if (!username) {
-    throw new functions.https.HttpsError("invalid-argument", "Missing username");
-  }
-  if (!userType) {
-    throw new functions.https.HttpsError("invalid-argument", "Missing userType");
-  }
-  if (!name) {
-    throw new functions.https.HttpsError("invalid-argument", "Missing name");
-  }
-
-  const usernameDocRef = db.collection("usernames").doc(username);
-  const userDocRef = db.collection("users").doc(userId);
-
-  // Firestore transaction to ensure atomic write
   try {
-    await db.runTransaction(async (transaction) => {
-      const usernameDoc = await transaction.get(usernameDocRef);
-      if (usernameDoc.exists) {
-        // eslint-disable-next-line max-len
-        throw new UsernameAlreadyTakenError();
-      }
+    const {userId, username, userType, name, phoneNumber, email} = request.data;
+    if (!userId || !username || !userType || !name || (!phoneNumber && !email)) {
+      throw new MissingParametersError();
+    } else if (userId !== request.auth?.uid || 
+              (phoneNumber !== '' && phoneNumber !== request.auth?.token?.phone_number) || 
+              (email !== '' && email !== request.auth?.token?.email) ||
+              username.length < 6 || username.length > 15 || !/^[a-z0-9_]+$/.test(username) ||
+              name.length < 3 || name.length > 30 || !/^[a-zA-Z_ ]+$/.test(name)) {
+      throw new NotValidUserDataError();
+    }
 
-      const userDoc = await transaction.get(userDocRef);
-      if (userDoc.exists) {
-        throw new UserAlreadyExistsError();
-      }
+    const usernameDocRef = db.collection("usernames").doc(username);
+    const userDocRef = db.collection("users").doc(userId);
+    const privateDataDocRef = userDocRef.collection("private").doc("privateData");
 
-      // Create username document
-      transaction.set(usernameDocRef, {userId});
+    let createdAt: FirebaseFirestore.Timestamp;
+    let setAt: FirebaseFirestore.Timestamp;
 
-      // Create user document
-      transaction.set(userDocRef, {
-        userType,
-        name,
-        username,
-        createdAt: admin.firestore.FieldValue.serverTimestamp(),
+    createdAt = admin.firestore.Timestamp.now();
+    setAt = createdAt;
+    
+
+    // Firestore transaction to ensure atomic write
+    try {
+      await db.runTransaction(async (transaction) => {
+        const usernameDoc = await transaction.get(usernameDocRef);
+        if (usernameDoc.exists) {
+          throw new UsernameAlreadyTakenError();
+        }
+
+        const userDoc = await transaction.get(userDocRef);
+        if (userDoc.exists) {
+          throw new UserAlreadyExistsError();
+        }
+
+        // Create username document
+        transaction.set(usernameDocRef, {userId, 
+          setAt});
+        
+        // Create user document
+        transaction.set(userDocRef, {
+          userType,
+          name,
+          username,
+          createdAt,
+        });
+
+        // Create private data document
+        transaction.set(privateDataDocRef, {
+          userType,
+          phoneNumber,
+          email,
+          isProfileCompleted: false,
+        });
+        
       });
-    });
-    return {message: "UserAndUsernameCreatedSuccessfully"};
+
+      return { 
+        status: "success", 
+        createdAt: createdAt.toDate().toISOString()
+      };
+    } catch (error) {
+      throw error;
+    }
   } catch (error) {
-    if (error instanceof UsernameAlreadyTakenError) {
+    if (error instanceof MissingParametersError) {
+      throw new functions.https.HttpsError("invalid-argument", error.name);
+    } else if (error instanceof UsernameAlreadyTakenError) {
       throw new functions.https.HttpsError("already-exists", error.name);
     } else if (error instanceof UserAlreadyExistsError) {
       throw new functions.https.HttpsError("already-exists", error.name);
+    } else if (error instanceof NotValidUserDataError) {
+      throw new functions.https.HttpsError("invalid-argument", error.name);
     } else {
-      // eslint-disable-next-line max-len
       throw new functions.https.HttpsError("internal", "An unknown error occurred");
     }
   }
 });
-
